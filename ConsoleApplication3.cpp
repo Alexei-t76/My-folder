051e313797636b86fe1efdcc909e26056ff8882b
Alexei-t76 <116583001+Alexei-t76@users.noreply.github.com>
2022-10-25 21:47:25 +0700

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    system("chcp 1251");
+
+    cout << "\n";
+    cout << "\tКаждый\n";
+    cout << "\t\tохотник\n";
+    cout << "\t\t\tжелает\n";
+    cout << "\t\t\t\tзнать,\n";
+    cout << "\t\t\t\t\tгде\n";
+    cout << "\t\t\t\t\t\tсидит\n";
+    cout << "\t\t\t\t\t\t\tфазан\n\n";
+
+    return 0;
+}
