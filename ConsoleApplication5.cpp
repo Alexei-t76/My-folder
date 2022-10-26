9501744f6d52546130f87f55e2cf52480e1e22ea
Alexei-t76 <116583001+Alexei-t76@users.noreply.github.com>
2022-10-26 20:37:15 +0700

    Add files via upload

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    float R1 = 2, R2 = 4, R3 = 8, R0;
+    R0 = 1 / (1 / R1 + 1 / R2 + 1 / R3);
+    // вывод результата.
+    cout << "\n";
+    cout << "\tR0 = " << R0 << "\n";
+
+    return 0;
+}
