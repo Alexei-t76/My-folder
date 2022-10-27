51d96f2d6eea71b79e82e477b652c96ac285533a
Alexei-t76 <116583001+Alexei-t76@users.noreply.github.com>
2022-10-27 18:38:48 +0700

    Add files via upload

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    system("chcp 1251");
+
+    float s, t, v;
+    
+    cout << "\n";
+    cout << "\tведите расстояние до аэропорта(км.)\n";
+    cin >> s;
+    cout << "\tведите время, за которое Вам нужно доехать(ч.)\n";
+    cin >> t;
+
+    v = s / t;
+
+    cout << "\tВаша скорость должна быть не менее " << v << " км/ч.\n\n";
+    
+    return 0;
+}
